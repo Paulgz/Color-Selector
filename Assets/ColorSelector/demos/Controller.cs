f61c46ef75dc415964f6af6cd0424ec828393e18
paul qf gaze <paulgaze@gmail.com>
2025-09-20 12:21:09 +0100

    put demo code in ColorTools namespace

--- Assets/ColorSelector/demos/Controller.cs
+++ Assets/ColorSelector/demos/Controller.cs
@@ -1,35 +1,35 @@
-using ColorTools;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
-{
-    public Camera myCamera;
-    public ColorSelector colorSelector;
-    public GameObject   activateButton;
+namespace ColorTools {
+    public class Controller : MonoBehaviour {
+        public Camera myCamera;
+        public ColorSelector colorSelector;
+        public GameObject   activateButton;
 
-    private Color startColor;
+        private Color startColor;
 
-    public void buttonSelectColor()
-    {
-        startColor = myCamera.backgroundColor;
-        colorSelector.setTitle("Select Background Color");
-        colorSelector.gameObject.SetActive(true);
-        colorSelector.setColor(startColor);
-        activateButton.SetActive(false);
-    }
-    public void colorOkay()
-    {
-        activateButton.SetActive(true);
-        colorSelector.gameObject.SetActive(false);
-    }
-    public void colorCancel()
-    {
-        myCamera.backgroundColor = startColor;
-        activateButton.SetActive(true);
-        colorSelector.gameObject.SetActive(false);
-    }
-    public void colorChange(Color newColor)
-    {
-        myCamera.backgroundColor = newColor;
+        public void buttonSelectColor()
+        {
+            startColor = myCamera.backgroundColor;
+            colorSelector.setTitle("Select Background Color");
+            colorSelector.gameObject.SetActive(true);
+            colorSelector.setColor(startColor);
+            activateButton.SetActive(false);
+        }
+        public void colorOkay()
+        {
+            activateButton.SetActive(true);
+            colorSelector.gameObject.SetActive(false);
+        }
+        public void colorCancel()
+        {
+            myCamera.backgroundColor = startColor;
+            activateButton.SetActive(true);
+            colorSelector.gameObject.SetActive(false);
+        }
+        public void colorChange(Color newColor)
+        {
+            myCamera.backgroundColor = newColor;
+        }
     }
 }
